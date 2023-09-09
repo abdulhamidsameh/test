e742c1d9d88f8cd7ebc519bc73718f677647f0b7
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2023-09-09 18:26:03 +0300

    Upload

---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.css'],
+})
+export class PortfolioComponent {
+  // clicked: boolean = true;
+  // arr: string[] = [
+  //   '../../assets/images/poert1.png',
+  //   '../../assets/images/port2.png',
+  //   '../../assets/images/port3.png',
+  // ];
+  // imgPath: string = '';
+  // click(event: any) {
+  //   this.imgPath = this.arr[event];
+  // }
+}
